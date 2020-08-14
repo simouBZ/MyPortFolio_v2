6d6a37f449bce1f73df3c5ca50aa7e6b96be2001
MedBZ <simou.boulhez@gmail.com>
2020-08-14 23:26:41 +0100

    Ajoutez des fichiers projet.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Core.Entities
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        public Adress Adress { get; set; }
+    }
+}
